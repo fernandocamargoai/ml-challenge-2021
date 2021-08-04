992965a711713c2204220f94ff7cfe77c1f5573f
Fernando Camargo <fernando.camargo.ai@gmail.com>
2021-08-04 18:12:42 -0300

    fix: ENABLE_MIXED_PRECISION

--- train.py
+++ train.py
@@ -17,33 +17,40 @@ from ml_challenge.task.training import DeepARTraining
 @click.option("--batch_size", type=int)
 @click.option("--lr", type=float)
 def train(
-        gradient_clip_val: float,
-        context_length: int,
-        weight_decay: float,
-        dropout_rate: float,
-        hidden_size: int,
-        num_layers: int,
-        batch_size: int,
-        lr: float,
-    ):
+    gradient_clip_val: float,
+    context_length: int,
+    weight_decay: float,
+    dropout_rate: float,
+    hidden_size: int,
+    num_layers: int,
+    batch_size: int,
+    lr: float,
+):
     if "WANDB_KEY" in os.environ:
         wandb.login(key=os.environ["WANDB_KEY"])
-    luigi.build([DeepARTraining(
-        use_gpu=True,
-        validate_with_non_testing_skus=True,
-        num_workers=3,
-        embedding_dimension=[2, 3, 2, 2, 2, 8, 16, 16],
-        gradient_clip_val=gradient_clip_val,
-        context_length=context_length,
-        weight_decay=weight_decay,
-        dropout_rate=dropout_rate,
-        hidden_size=hidden_size,
-        num_layers=num_layers,
-        batch_size=batch_size,
-        lr=lr,
-        generate_submission="TRAINML_DATA_PATH" in os.environ,
-        precision=16 if os.environ.get("ENABLE_MIXED_PRECISION", False) else 32,
-    )], local_scheduler=True)
+    luigi.build(
+        [
+            DeepARTraining(
+                use_gpu=True,
+                validate_with_non_testing_skus=True,
+                num_workers=3,
+                embedding_dimension=[2, 3, 2, 2, 2, 8, 16, 16],
+                gradient_clip_val=gradient_clip_val,
+                context_length=context_length,
+                weight_decay=weight_decay,
+                dropout_rate=dropout_rate,
+                hidden_size=hidden_size,
+                num_layers=num_layers,
+                batch_size=batch_size,
+                lr=lr,
+                generate_submission="TRAINML_DATA_PATH" in os.environ,
+                precision=16
+                if os.environ.get("ENABLE_MIXED_PRECISION", "False") == "True"
+                else 32,
+            )
+        ],
+        local_scheduler=True,
+    )
 
 
 if __name__ == "__main__":
