ef134f4ef0f946ccf10505b4f2f5839e16e0bf8c
Fernando Camargo <fernando.camargo.ai@gmail.com>
2021-08-05 12:37:23 -0300

    chore: changes get_assets_path() to use a new env variable

--- ml_challenge/path.py
+++ ml_challenge/path.py
@@ -2,7 +2,5 @@ import os
 
 
 def get_assets_path() -> str:
-    if "TRAINML_DATA_PATH" in os.environ:
-        return os.path.join(os.environ["TRAINML_DATA_PATH"], "assets")
-    else:
-        return "assets"
+    return os.environ.get("ASSETS_PATH", "assets")
+
