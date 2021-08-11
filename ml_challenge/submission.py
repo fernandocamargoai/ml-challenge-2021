9fefb51afa96841414b992f1e049ee76e3c47638
Fernando Camargo <fernando.camargo.ai@gmail.com>
2021-08-11 15:07:58 -0300

    chore: removes "with_quantiles" method and splits the submission generation in two steps

--- ml_challenge/submission.py
+++ ml_challenge/submission.py
@@ -1,142 +1,18 @@
-import functools
-import gzip
-import os
-from multiprocessing.dummy import Pool
-from typing import List, Tuple, Callable, Dict
+from typing import List, Tuple, Callable
 
 import numpy as np
-import pandas as pd
 import tweedie
-import pytorch_lightning as pl
-from gluonts.dataset.common import Dataset
 from gluonts.model.forecast import SampleForecast
-
-from gluonts.torch.model.predictor import PyTorchPredictor
 from scipy.stats import norm, beta
 from statsmodels.distributions import ECDF
-from tqdm import tqdm
-
-from ml_challenge.lookahead_generator import LookaheadGenerator
 
 
-def _pool_forecast_transform_fn(
+def pool_forecast_transform_fn(
     input_: Tuple[SampleForecast, int],
     forecast_transform_fn: Callable[[SampleForecast, int], List[float]],
 ) -> List[float]:
     forecast, stock = input_
     return forecast_transform_fn(forecast, stock)
-
-
-def cdf_to_probas(cdf: List[float]) -> List[float]:
-    prob_array = np.array(np.ediff1d(cdf, to_begin=cdf[0]))
-    return list(prob_array / np.sum(prob_array))
-
-
-def generate_submission(
-    predictor: PyTorchPredictor,
-    num_samples: int,
-    seed: int,
-    dataset: Dataset,
-    df: pd.DataFrame,
-    task_path: str,
-    forecast_transform_fn: Callable[[SampleForecast, int], List[float]],
-    suffix: str,
-) -> str:
-    pl.seed_everything(seed, workers=True)
-
-    predictor.batch_size = 512
-    with Pool(os.cpu_count()) as pool:
-        with LookaheadGenerator(
-            predictor.predict(dataset, num_samples=num_samples)
-        ) as forecasts:
-            all_probas = list(
-                tqdm(
-                    pool.imap(
-                        functools.partial(
-                            _pool_forecast_transform_fn,
-                            forecast_transform_fn=forecast_transform_fn,
-                        ),
-                        zip(forecasts, df["target_stock"]),
-                    ),
-                    total=len(df),
-                )
-            )
-    all_probas = [
-        probas if sum(probas) > 0 else ([0.0] * (len(probas) - 1) + [1.0])
-        for probas in all_probas
-    ]
-
-    submission_path = os.path.join(
-        task_path, f"submission_{suffix}_num-samples={num_samples}_seed={seed}.csv.gz"
-    )
-    with gzip.open(submission_path, "wb") as f:
-        for probas in all_probas:
-            f.write(
-                (",".join([str(round(proba, 4)) for proba in probas]) + "\n").encode(
-                    "utf-8"
-                )
-            )
-    return submission_path
-
-
-def calculate_out_of_stock_cdf_with_quantiles(
-    forecast: SampleForecast, stock: int, quantiles: List[str]
-) -> List[float]:
-    quantile_forecast = forecast.to_quantile_forecast(quantiles)
-    out_of_stock = np.cumsum(quantile_forecast.forecast_array, axis=1) >= stock
-    return [
-        (1 - float(quantile_forecast.forecast_keys[out_of_stock_index]))
-        if out_of_stock_num > 0
-        else 0.0
-        for out_of_stock_num, out_of_stock_index in zip(
-            out_of_stock.sum(axis=0), np.argmax(out_of_stock, axis=0)
-        )
-    ]
-
-
-def forecast_with_quantiles(
-    forecast: SampleForecast, stock: int, quantiles: List[str]
-) -> List[float]:
-    cdf = calculate_out_of_stock_cdf_with_quantiles(forecast, stock, quantiles)
-    return cdf_to_probas(cdf)
-
-
-def generate_submission_with_quantiles(
-    predictor: PyTorchPredictor,
-    num_samples: int,
-    seed: int,
-    dataset: Dataset,
-    df: pd.DataFrame,
-    task_path: str,
-    quantiles: List[float] = None,
-) -> str:
-    if quantiles is None:
-        quantiles = [str(q) for q in np.arange(0.00, 1.01, 0.01)]
-
-    forecast_transform_fn = functools.partial(
-        forecast_with_quantiles, quantiles=quantiles
-    )
-
-    return generate_submission(
-        predictor,
-        num_samples,
-        seed,
-        dataset,
-        df,
-        task_path,
-        forecast_transform_fn,
-        "with_quantiles",
-    )
-
-
-@functools.lru_cache
-def apply_tweedie(
-    out_of_stock_days: int, phi: float = 2.0, power=1.3, total_days: int = 30
-) -> List[float]:
-    distro = tweedie.tweedie(p=power, mu=out_of_stock_days, phi=phi)
-
-    cdf = [distro.cdf(i) for i in range(1, total_days + 1)]
-    return cdf_to_probas(cdf)
 
 
 def calculate_out_of_stock_days_from_samples(
@@ -149,126 +25,36 @@ def calculate_out_of_stock_days_from_samples(
     return sample_days + 1
 
 
-def forecast_with_tweedie(
-    forecast: SampleForecast,
-    stock: int,
-    phi: float = 2.0,
-    power=1.3,
-    total_days: int = 30,
+def cdf_to_probas(cdf: List[float]) -> List[float]:
+    prob_array = np.array(np.ediff1d(cdf, to_begin=cdf[0]))
+    return list(prob_array / np.sum(prob_array))
+
+
+def apply_tweedie(
+    sample_days: np.ndarray, phi: float = 2.0, power=1.3, total_days: int = 30
 ) -> List[float]:
-    sample_days = calculate_out_of_stock_days_from_samples(forecast, stock)
-    return apply_tweedie(
-        round(sample_days.mean()), phi=phi, power=power, total_days=total_days
-    )
-
-
-def generate_submission_with_tweedie(
-    predictor: PyTorchPredictor,
-    num_samples: int,
-    seed: int,
-    dataset: Dataset,
-    df: pd.DataFrame,
-    task_path: str,
-    phi: float = 2.0,
-    power: float = 1.3,
-    total_days: int = 30,
-) -> str:
-    forecast_transform_fn = functools.partial(
-        forecast_with_tweedie, phi=phi, power=power, total_days=total_days,
-    )
-
-    return generate_submission(
-        predictor,
-        num_samples,
-        seed,
-        dataset,
-        df,
-        task_path,
-        forecast_transform_fn,
-        f"with_tweedie_phi={phi}_power={power}",
-    )
-
-
-def apply_normal(days_mean: float, days_std: float, total_days: int = 30):
-    distro = norm(days_mean, days_std)
+    distro = tweedie.tweedie(p=power, mu=sample_days.mean(), phi=phi)
 
     cdf = [distro.cdf(i) for i in range(1, total_days + 1)]
     return cdf_to_probas(cdf)
 
 
-def forecast_with_normal(
-    forecast: SampleForecast, stock: int, total_days: int = 30
-) -> List[float]:
-    sample_days = calculate_out_of_stock_days_from_samples(forecast, stock)
-    return apply_normal(sample_days.mean(), sample_days.std(), total_days=total_days)
+def apply_normal(sample_days: np.ndarray, total_days: int = 30):
+    distro = norm(sample_days.mean(), sample_days.std())
+
+    cdf = [distro.cdf(i) for i in range(1, total_days + 1)]
+    return cdf_to_probas(cdf)
 
 
-def generate_submission_with_normal(
-    predictor: PyTorchPredictor,
-    num_samples: int,
-    seed: int,
-    dataset: Dataset,
-    df: pd.DataFrame,
-    task_path: str,
-    total_days: int = 30,
-) -> str:
-    forecast_transform_fn = functools.partial(
-        forecast_with_normal, total_days=total_days,
-    )
-
-    return generate_submission(
-        predictor,
-        num_samples,
-        seed,
-        dataset,
-        df,
-        task_path,
-        forecast_transform_fn,
-        "with_normal",
-    )
-
-
-def apply_ecdf(sample_days: List[int], total_days: int = 30):
+def apply_ecdf(sample_days: np.ndarray, total_days: int = 30):
     ecdf = ECDF(sample_days)
     cdf = [ecdf(i) for i in range(1, total_days + 1)]
     return cdf_to_probas(cdf)
 
 
-def forecast_with_ecdf(
-    forecast: SampleForecast, stock: int, total_days: int = 30
-) -> List[float]:
-    sample_days = calculate_out_of_stock_days_from_samples(forecast, stock)
-    return apply_ecdf(sample_days, total_days=total_days)
-
-
-def generate_submission_with_ecdf(
-    predictor: PyTorchPredictor,
-    num_samples: int,
-    seed: int,
-    dataset: Dataset,
-    df: pd.DataFrame,
-    task_path: str,
-    total_days: int = 30,
-) -> str:
-    forecast_transform_fn = functools.partial(
-        forecast_with_ecdf, total_days=total_days,
-    )
-
-    return generate_submission(
-        predictor,
-        num_samples,
-        seed,
-        dataset,
-        df,
-        task_path,
-        forecast_transform_fn,
-        "with_ecdf",
-    )
-
-
-def apply_beta(days_mean: float, days_std: float, total_days: int = 30):
-    mu = days_mean / total_days
-    var = (days_std ** 2) / total_days
+def apply_beta(sample_days: np.ndarray, total_days: int = 30):
+    mu = sample_days.mean() / total_days
+    var = (sample_days.std() ** 2) / total_days
     a = ((1 - mu) / var - 1 / mu) * mu ** 2
     b = a * (1 / mu - 1)
 
@@ -276,35 +62,3 @@ def apply_beta(days_mean: float, days_std: float, total_days: int = 30):
 
     cdf = [distro.cdf(i / total_days) for i in range(1, total_days + 1)]
     return cdf_to_probas(cdf)
-
-
-def forecast_with_beta(
-    forecast: SampleForecast, stock: int, total_days: int = 30
-) -> List[float]:
-    sample_days = calculate_out_of_stock_days_from_samples(forecast, stock)
-    return apply_normal(sample_days.mean(), sample_days.std(), total_days=total_days)
-
-
-def generate_submission_with_beta(
-    predictor: PyTorchPredictor,
-    num_samples: int,
-    seed: int,
-    dataset: Dataset,
-    df: pd.DataFrame,
-    task_path: str,
-    total_days: int = 30,
-) -> str:
-    forecast_transform_fn = functools.partial(
-        forecast_with_beta, total_days=total_days,
-    )
-
-    return generate_submission(
-        predictor,
-        num_samples,
-        seed,
-        dataset,
-        df,
-        task_path,
-        forecast_transform_fn,
-        "with_beta",
-    )
--- ml_challenge/task/submission.py
+++ ml_challenge/task/submission.py
@@ -1,25 +1,86 @@
+import functools
+import gzip
 import json
 import os
 from functools import cached_property
+from multiprocessing import Pool
 
 import luigi
 import numpy as np
 import torch
+import pytorch_lightning as pl
+from tqdm import tqdm
 
+from ml_challenge.lookahead_generator import LookaheadGenerator
 from ml_challenge.submission import (
-    generate_submission_with_quantiles,
-    generate_submission_with_tweedie,
-    generate_submission_with_normal,
-    generate_submission_with_ecdf, generate_submission_with_beta,
+    pool_forecast_transform_fn,
+    calculate_out_of_stock_days_from_samples,
+    apply_tweedie,
+    apply_normal,
+    apply_ecdf,
+    apply_beta,
 )
 from ml_challenge.task.training import DeepARTraining
+
+
+class GenerateOutOfStockDaySamplePredictions(luigi.Task):
+    task_path: str = luigi.Parameter()
+
+    num_samples: int = luigi.IntParameter(default=100)
+    seed: int = luigi.IntParameter(default=42)
+
+    @cached_property
+    def training(self) -> DeepARTraining:
+        with open(os.path.join(self.task_path, "params.json"), "r") as params_file:
+            params = json.load(params_file)
+        return DeepARTraining(**params)
+
+    def output(self):
+        return luigi.LocalTarget(
+            os.path.join(
+                self.task_path,
+                f"out_of_stock_day_sample_predictions_num-samples={self.num_samples}_seed={self.seed}.npy",
+            )
+        )
+
+    def run(self):
+        predictor = self.training.get_trained_predictor(torch.device("cuda"))
+
+        pl.seed_everything(self.seed, workers=True)
+
+        predictor.batch_size = 512
+        with Pool(os.cpu_count()) as pool:
+            with LookaheadGenerator(
+                predictor.predict(
+                    self.training.test_dataset, num_samples=self.num_samples
+                )
+            ) as forecasts:
+                all_days_samples = list(
+                    tqdm(
+                        pool.imap(
+                            functools.partial(
+                                pool_forecast_transform_fn,
+                                forecast_transform_fn=calculate_out_of_stock_days_from_samples,
+                            ),
+                            zip(forecasts, self.training.test_df["target_stock"]),
+                        ),
+                        total=len(self.training.test_df),
+                    )
+                )
+
+        np.save(self.output().path, np.array(all_days_samples))
 
 
 class GenerateSubmission(luigi.Task):
     task_path: str = luigi.Parameter()
 
-    method: str = luigi.ChoiceParameter(
-        choices=["with_quantiles", "with_tweedie", "with_normal", "with_ecdf", "with_beta"]
+    distribution: str = luigi.ChoiceParameter(
+        choices=[
+            "tweedie",
+            "normal",
+            "ecdf",
+            "beta",
+        ]
     )
 
     quantile_step: float = luigi.FloatParameter(default=0.01)
@@ -30,63 +91,50 @@ class GenerateSubmission(luigi.Task):
 
     seed: int = luigi.IntParameter(default=42)
 
-    @cached_property
-    def training(self) -> DeepARTraining:
-        with open(os.path.join(self.task_path, "params.json"), "r") as params_file:
-            params = json.load(params_file)
-        return DeepARTraining(**params)
+    def requires(self):
+        return GenerateOutOfStockDaySamplePredictions(
+            task_path=self.task_path, num_samples=self.num_samples, seed=self.seed,
+        )
+
+    def output(self):
+        return luigi.LocalTarget(
+            os.path.join(
+                self.task_path,
+                f"submission_{self.distribution}_num-samples={self.num_samples}_seed={self.seed}.csv.gz",
+            )
+        )
 
     def run(self):
-        predictor = self.training.get_trained_predictor(torch.device("cuda"))
+        out_of_stock_day_sample_preds = np.load(self.input().path)
 
-        if self.method == "with_quantiles":
-            quantiles = [
-                q for q in np.arange(0.00, 1.0 + self.quantile_step, self.quantile_step)
-            ]
-            generate_submission_with_quantiles(
-                predictor,
-                self.num_samples,
-                self.seed,
-                self.training.test_dataset,
-                self.training.test_df,
-                self.training.output().path,
-                quantiles,
+        if self.distribution == "tweedie":
+            apply_dist_fn = functools.partial(
+                apply_tweedie, phi=self.tweedie_phi, power=self.tweedie_power
             )
-        elif self.method == "with_tweedie":
-            generate_submission_with_tweedie(
-                predictor,
-                self.num_samples,
-                self.seed,
-                self.training.test_dataset,
-                self.training.test_df,
-                self.training.output().path,
-                phi=self.tweedie_phi,
-                power=self.tweedie_power,
+        elif self.distribution == "normal":
+            apply_dist_fn = apply_normal
+        elif self.distribution == "ecdf":
+            apply_dist_fn = apply_ecdf
+        else:  # self.distribution == "beta":
+            apply_dist_fn = apply_beta
+
+        with Pool(os.cpu_count()) as pool:
+            all_probas = list(
+                tqdm(
+                    pool.imap(apply_dist_fn, out_of_stock_day_sample_preds,),
+                    total=out_of_stock_day_sample_preds.shape[0],
+                )
             )
-        elif self.method == "with_normal":
-            generate_submission_with_normal(
-                predictor,
-                self.num_samples,
-                self.seed,
-                self.training.test_dataset,
-                self.training.test_df,
-                self.training.output().path,
-            )
-        elif self.method == "with_ecdf":
-            generate_submission_with_ecdf(
-                predictor,
-                self.num_samples,
-                self.seed,
-                self.training.test_dataset,
-                self.training.test_df,
-                self.training.output().path,
-            )
-        elif self.method == "with_beta":
-            generate_submission_with_beta(
-                predictor,
-                self.num_samples,
-                self.seed,
-                self.training.test_dataset,
-                self.training.test_df,
-                self.training.output().path,
-            )
+
+        all_probas = [
+            probas if sum(probas) > 0 else ([0.0] * (len(probas) - 1) + [1.0])
+            for probas in all_probas
+        ]
+
+        with gzip.open(self.output().path, "wb") as f:
+            for probas in all_probas:
+                f.write(
+                    (
+                        ",".join([str(round(proba, 4)) for proba in probas]) + "\n"
+                    ).encode("utf-8")
+                )
